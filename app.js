90f4d72c2ccbd76936697cc8327021f84776ae49
Alibuba1 <bulbsher@mail.ru>
2021-12-13 22:59:20 +0100

    chore: add first task

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
-console.log(1);
+let numberOfFilms = prompt("Сколько фильмов вы уже посмотрели?","");
+
+let personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false,
+};
+
+let lastmovie = prompt("Один из последних просмотренных фильмов?"),
+    grade = prompt("На сколько оцените его?"),
+    lastmovie1 = prompt("Один из последних просмотренных фильмов?"),
+    grade1 = prompt("На сколько оцените его?");
+
+personalMovieDB.movies[lastmovie] = grade;
+personalMovieDB.movies[lastmovie1] = grade1;
+console.log(personalMovieDB);
