483c50c7d004ea91492898a62875ec323a11a61a
Alibuba1 <bulbsher@mail.ru>
2021-12-26 13:10:19 +0100

    fix: second task

--- app.js
+++ app.js
@@ -17,58 +17,58 @@
 // personalMovieDB.movies[lastmovie1] = grade1;
 // console.log(personalMovieDB);
 
-let numberOfFilms = prompt("Сколько фильмов вы уже посмотрели?", "");
+// let numberOfFilms = prompt("Сколько фильмов вы уже посмотрели?", "");
 
-let personalMovieDB = {
-    count: numberOfFilms,
-    movies: {},
-    actors: {},
-    genres: [],
-    privat: false,
-};
+// let personalMovieDB = {
+//     count: numberOfFilms,
+//     movies: {},
+//     actors: {},
+//     genres: [],
+//     privat: false,
+// };
 
-for ( let i = 0; i < 2; i++) {
-    let lastmovie = prompt("Один из последних просмотренных фильмов?",""),
-        grade = prompt("На сколько оцените его?","");
+// for ( let i = 0; i < 2; i++) {
+//     let lastmovie = prompt("Один из последних просмотренных фильмов?",""),
+//         grade = prompt("На сколько оцените его?","");
     
-    if (lastmovie != null && grade != null && lastmovie != "" && grade != "" && grade.length < 50) {
-        personalMovieDB.movies[lastmovie] = grade;
-    } else {
-        i--;
-    }
-}
+//     if (lastmovie != null && grade != null && lastmovie != "" && grade != "" && grade.length < 50) {
+//         personalMovieDB.movies[lastmovie] = grade;
+//     } else {
+//         i--;
+//     }
+// }
 
-if (personalMovieDB.count < 10) {
-    alert ("Просмотрено довольно мало фильмов");
-} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
-    alert ("Вы классический зритель");
-} else if (personalMovieDB.count >= 30) {
-    alert ("Вы киноман");
-} else {
-    alert ("Произошла ошибка");
-}
+// if (personalMovieDB.count < 10) {
+//     alert ("Просмотрено довольно мало фильмов");
+// } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+//     alert ("Вы классический зритель");
+// } else if (personalMovieDB.count >= 30) {
+//     alert ("Вы киноман");
+// } else {
+//     alert ("Произошла ошибка");
+// }
 
-let j = 0;
-while (j < 2) {
-    let lastmovie = prompt("Один из последних просмотренных фильмов?",""),
-        grade = prompt("На сколько оцените его?","");
-    j++;
+// let j = 0;
+// while (j < 2) {
+//     let lastmovie = prompt("Один из последних просмотренных фильмов?",""),
+//         grade = prompt("На сколько оцените его?","");
+//     j++;
 
-    if (lastmovie != null && grade != null && lastmovie != "" && grade != "" && grade.length < 50) {
-        personalMovieDB.movies[lastmovie] = grade;
-    } else {
-        j--;
-    }
-}
+//     if (lastmovie != null && grade != null && lastmovie != "" && grade != "" && grade.length < 50) {
+//         personalMovieDB.movies[lastmovie] = grade;
+//     } else {
+//         j--;
+//     }
+// }
 
-do {
-        let lastmovie = prompt("Один из последних просмотренных фильмов?",""),
-        grade = prompt("На сколько оцените его?","");
-    j++;
+// do {
+//         let lastmovie = prompt("Один из последних просмотренных фильмов?",""),
+//         grade = prompt("На сколько оцените его?","");
+//     j++;
 
-    if (lastmovie != null && grade != null && lastmovie != "" && grade != "" && grade.length < 50) {
-        personalMovieDB.movies[lastmovie] = grade;
-    } else {
-        j--;
-    }
-} while ( j < 2);
+//     if (lastmovie != null && grade != null && lastmovie != "" && grade != "" && grade.length < 50) {
+//         personalMovieDB.movies[lastmovie] = grade;
+//     } else {
+//         j--;
+//     }
+// } while ( j < 2);
